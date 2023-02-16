2325573405f0114c78714f846a6a72c1081b6692
Al0cam <benjaminsikac@gmail.com>
2023-02-16 18:03:38 +0100

    background change and flex games

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="flex flex-col center items-center bg-slate-500">
+  <div class="flex flex-col justify-center h-screen center items-center bg-slate-500">
     <div>
-      <span class="text-3xl font-bold underline"> Hello world! </span>
+      <span class="text-3xl font-bold"> Hello world! </span>
     </div>
   </div>
 </template>
