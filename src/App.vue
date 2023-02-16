e74214a93fabda4d4ceda258527dbbea91fde7fa
Al0cam <benjaminsikac@gmail.com>
2023-02-16 17:56:55 +0100

    using tailwind, vite icon missing for in-editor work

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
-<script setup lang="ts">
-</script>
-
+<script setup lang="ts"></script>
 
 <template>
-  <span class="text-3xl font-bold underline">
-    Hello world!
-  </span>
+  <div class="flex flex-col center items-center bg-slate-500">
+    <div>
+      <span class="text-3xl font-bold underline"> Hello world! </span>
+    </div>
+  </div>
 </template>
