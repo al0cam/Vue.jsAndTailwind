51764bf07a62b1f001f2f89465eb5f445e150230
Al0cam <benjaminsikac@gmail.com>
2023-02-24 21:24:14 +0100

    added scale to button in navbar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,27 @@
 <script setup lang="ts"></script>
 
+// TODO: write custom classes for tailwindcss for flex style
+
 <template>
-  <div class="flex flex-col justify-center h-screen center items-center bg-slate-500">
-    <div>
-      <span class="text-3xl font-bold"> Hello world! </span>
+  <div class="flex flex-col h-screen w-screen items-center bg-slate-500 relative">
+    <div class="border-b w-full flex flex-row">
+      <button class="text-3xl font-bold static border-r p-2 hover:scale-110">
+        Item 1
+      </button>
+      <span class="text-3xl font-bold static border-r p-2"> Item 2 </span>
+      <span class="text-3xl font-bold static border-r p-2"> Item 3 </span>
+      <span class="text-3xl font-bold static border-r p-2"> Item 4 </span>
+    </div>
+
+    <div class="w-full">
+      <span class="text-3xl font-bold relative"> Hello world! </span>
     </div>
   </div>
 </template>
+
+<style scoped>
+.class {
+  color: red;
+  text-align: center;
+}
+</style>
